2bc81d50ab3ba3280097393a3071777edbc419fb
Timo Sand <timo.sand@kapsi.fi>
2010-03-21 17:33:21 +0200

    Assignment 5 done

---
+++ Ex1/grepFile.scala
@@ -0,0 +1,31 @@
+import scala.io.Source
+
+class GrepFile(filename: String) {
+    val file = Source.fromFile(filename)
+    var fileRepeater = file
+    var searchString = ""
+    val question = "What string do you want to look for in file " + filename + "?\n"
+    var count = 0
+    do {
+        searchString = Console.readLine(question)
+        count = 0
+        for (line <- fileRepeater.getLines) {
+            count += 1
+            if (searchString != "" && line.containsSlice(searchString)) {
+                print(count + ": " + line)
+            }
+        }
+        fileRepeater = fileRepeater.reset
+    } while (searchString != "") 
+}
+
+object GrepRunner {
+    def main(args: Array[String]) {
+        if (args.length > 0) {
+            val filename = args(0)
+            new GrepFile(filename)
+        } else {
+            Console.err.println("Please enter filename!")
+        }
+    }
+}
