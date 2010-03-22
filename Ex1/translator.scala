013d51f95560ec79984846c185e1554e8dee748f
Timo Sand <timo.sand@kapsi.fi>
2010-03-22 11:10:07 +0200

    Initali submit of Assignment 6

---
+++ Ex1/translator.scala
@@ -0,0 +1,24 @@
+import scala.collection.mutable.HashMap
+
+class Translator() {
+    var storage = new HashMap[String, List[String]]
+    
+    def put(key: String, value: String) {
+        var newVals = storage.getOrElseUpdate(key, List(""))
+        if (newVals != 1 && newVals(0) == "" ) {
+            newVals = value :: newVals
+        } else {
+            newVals = List(value)
+        }
+        storage.update(key, newVals)
+    }
+    
+    def get(key: String): List[String] = {
+        return storage(key)
+    }
+}
+
+object TranslatorRunner extends Application {
+    val temp = new Translator()
+    temp.put("lol", "nuubz")
+}
