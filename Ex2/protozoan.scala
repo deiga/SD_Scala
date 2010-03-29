90417df1ecdeecc0bddb1ab70200714de18b455f
Timo Sand <timo.sand@kapsi.fi>
2010-03-29 11:11:06 +0300

    finished ass11, only missing example of program

--- Ex2/protozoan.scala
+++ Ex2/protozoan.scala
@@ -14,17 +14,46 @@ class Protozoan() {
         var tmpGenome = new Array[Char](10)
         for (i <- 0 to 10) {
             tmpGenome(i) = randomNukleotide()
-        }
-        
+        }  
         return tmpGenome.toList
     }
     
     def randomNukleotide(): Char = {
+        val index = Math.random * 4
+        nukleotide(index.toInt)
+    }
+    
+    def split(): Protozoan = new Protozoan(genome)
+    
+    def mate(that: Protozoan): Protozoan = {
+        val newGenome = new Array[Char](10)
+        for (i <- 0 to 10) {
+            if (Math.random >= 0.5) {
+                newGenome(i) = genome(i)
+            } else {
+                newGenome(i) = that.genome(i)
+            }
+        }
+        new Protozoan(newGenome.toList)
+    }
+    
+    def identify(): Int = serial
+    
+    override def toString(): String = {
+        var rtrnString = "Serial: " + serial +"\nGenome: "
+        for (item <- genome) {
+            rtrnString += item + " "
+        }
         
+        return rtrnString
     }
     
 }
 
 object Protozoan {
     private var orderNumber = 0
+}
+
+object Geenilaboratorio extends Application {
+    
 }
