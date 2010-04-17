d8123be7572a79e828d3fa2ff22933b630a50594
Timo Sand <timo.sand@kapsi.fi>
2010-04-17 19:32:55 +0300

    ass12 done

--- Ex3/primes.scala
+++ Ex3/primes.scala
@@ -17,11 +17,22 @@ class Sieveb(limit: Int) {
    println(numbers)
 }
 
+class Sievec(min: Int, max: Int) {
+   var numbers = 2 to max toList
+   
+   for (i <- numbers; j <- 2 to max) {
+           numbers = numbers - i*j
+   }
+   println(numbers.filter(x => x >= min && x <= max))
+}
+
 object Test {
     def main(args: Array[String]) {
         println("a.) Anna luku kuinka monta alkulukua tuotetaan: ")
         new Sievea(Console.readInt)
         println("b.) Anna luku mihin saakka tuotetaan alkulukuja: ")
         new Sieveb(Console.readInt)
+        println("c.) Anna luku mistä lähtirn ja mihin saakka tuotetaan alkulukuja: ")
+        new Sievec(Console.readInt, Console.readInt)
     }
 }
